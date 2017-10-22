a2316a0bd26066f6333d02fef4a076f006813c9a
Andy Noblet <andrewbnoblet@gmail.com>
2017-10-22 09:46:19 -0400

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -32,7 +31,6 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    ToolbarComponent,
     HomeComponent,
     LoginComponent,
     RegisterComponent
