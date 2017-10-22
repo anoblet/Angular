35fdccbb24ab9719be9d7879aacd1c2b5d33e5cf
Andy Noblet <andrewbnoblet@gmail.com>
2017-10-22 13:14:55 -0400

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import {MatMenuModule} from '@angular/material';
 import {MatInputModule} from '@angular/material';
 import {MatButtonModule} from '@angular/material';
 import {MatIconModule} from '@angular/material';
+import {MatCheckboxModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './CMS/home/home.component';
+import { LoginComponent } from './CMS/login/login.component';
+import { RegisterComponent } from './CMS/register/register.component';
 
 const appRoutes: Routes = [
   { path: 'home',      component: HomeComponent },
@@ -24,13 +24,12 @@ const appRoutes: Routes = [
     redirectTo: '/home',
     pathMatch: 'full'
   },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
     HomeComponent,
     LoginComponent,
     RegisterComponent
@@ -42,6 +41,7 @@ const appRoutes: Routes = [
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatMenuModule,
     MatSidenavModule,
     MatCardModule,
